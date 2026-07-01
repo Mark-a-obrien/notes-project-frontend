839b5fc5165d07f6ad1d12d41632c4b3df4a64b4
Mark <84234570+Mark-a-obrien@users.noreply.github.com>
2026-07-01 14:10:20 +0100

    Update
    Added the js to its own file

---
+++ index.js
@@ -0,0 +1,21 @@
+const apiKey = "tqwvg4o8e9patu]gv32uy89gqweabcvsz";
+
+            fetch("http://127.0.0.1:8000/api/notes", {
+            method: "GET",
+            headers: {
+                "X-API-Key": apiKey,
+                "Content-Type": "application/json"
+            }
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                
+                data.forEach(element => {
+                    const note = document.createElement("li");
+                    note.textContent = element;
+                    document.getElementById("notes-list").appendChild(note);
+                });
+                
+            })
+            .catch(error => console.error("Error:", error));   
